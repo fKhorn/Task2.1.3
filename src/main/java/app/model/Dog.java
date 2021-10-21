7744586061a50d7779646e8afa48f9caddd3da06
fKhorn <khorn218@gmail.com>
2021-10-21 15:44:26 +0300

    commit 1

---
+++ src/main/java/app/model/Dog.java
@@ -0,0 +1,13 @@
+package app.model;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Dog extends Animal {
+
+    @Override
+    public String toString() {
+        return "I'm a dog!";
+    }
+}
